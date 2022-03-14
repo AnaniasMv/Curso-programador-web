7731e4107b83efd8cb329bdcea154a52cb346d22
MatheusV.A <ananiasmatheus22@outlook.com>
2022-03-14 08:50:28 -0300

    .

---
+++ POO/class.js
@@ -0,0 +1,22 @@
+class Calculadora {
+    constructor(v1, op, v2) {
+        this.v1 = v1
+        this.v2 = v2
+        this.op = op
+    }
+    calcular() {
+        if (op == '+') {
+            return parseFloat(this.v1) + parseFloat(this.v2)
+
+        } else if (op == '-') {
+            return parseFloat(this.v1) - parseFloat(this.v2)
+
+        } else if (op == '*') {
+            return parseFloat(this.v1) * parseFloat(this.v2)
+
+        } else if (op == '/') {
+            return parseFloat(this.v1) / parseFloat(this.v2)
+
+        }
+    }
+}
