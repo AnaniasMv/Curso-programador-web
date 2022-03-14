0e204ffc6cbef9110627b9d7604e8efd9e5e1379
MatheusV.A <ananiasmatheus22@outlook.com>
2022-03-14 08:51:47 -0300

    .

--- POO/class.js
+++ POO/class.js
@@ -20,3 +20,28 @@ class Calculadora {
         }
     }
 }
+
+var smarTv = new Produto('SmarTV 50 pol', 1500, 3600.50)
+smarTv.precoVenda = 1000
+console.log(smarTv.precoVenda)
+smarTv.precoVenda = 2200.60
+console.log(smarTv.precoVenda)
+class Produto {
+    constructor(descricao, precoCusto, precoVenda) {
+        this.descricao = descricao
+        this.precoCusto = precoCusto
+        this._precoVenda = precoVenda
+    }
+    get precoVenda() {
+        return this._precoVenda;
+    }
+    set precoVenda(valor) {
+        if (valor >= (this.precoCusto)) {
+            this._precoVenda = valor
+            console.log(`O preço de venda foi alterado para ${this._precoVenda}`)
+
+        } else {
+            console.log('O valor é muito baixo, não pode ser alterado')
+        }
+    }
+}
